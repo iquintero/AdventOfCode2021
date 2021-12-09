cde4b68fddb68250bebe8289538c9924d2de48cf
Ignacio Quintero <nacho950@gmail.com>
2021-12-09 03:56:41 +0000

    day2 part 1
    correct answer :)

--- src/day2.rs
+++ src/day2.rs
@@ -2,10 +2,22 @@ use std::fs::File;
 use std::io;
 
 pub fn day2(lines: io::Lines<io::BufReader<File>>) {
-  println!("Hello, Day2!");
+  let mut x = 0;
+  let mut y = 0;
   for line in lines {
     if let Ok(line) = line {
-      println!("{}", line);
+      let v: Vec<&str> = line.split(' ').collect();
+      let command = v[0];
+      let amount: i32 = v[1].trim().parse().expect("invalid number");
+
+      match command {
+        "down" =>  y += amount,
+        "up" => y -= amount,
+        "forward" => x += amount,
+        _ => {}
+      }     
     }
   }
+  let answer = x * y;
+  println!("answer: {}", answer);
 }
